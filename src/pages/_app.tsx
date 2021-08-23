e9eb48d9487873db3b4beb89afede3c8029f6beb
yuki_sakata <sakata_yuuki@waku-2.com>
2021-08-23 18:46:22 +0900

    add reset.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import '../../styles/globals.css';
+import 'modern-css-reset/dist/reset.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
