7b331513c11e4f6e7cd33aefb256f6475f00f69c
yuki_sakata <sakata_yuuki@waku-2.com>
2021-08-28 22:00:21 +0900

    MessageArea

---
+++ src/components/organisms/MessageArea/MessageArea.tsx
@@ -0,0 +1,47 @@
+import react from 'react';
+import { BotMessage } from '../../molecules/BotMessage';
+import { ReplyMessage } from '../../molecules/ReplyMessage';
+
+type BotMessage = {
+  messageType: 'bot';
+  text: string;
+};
+
+type ReplyMessage = {
+  messageType: 'reply';
+  type: string;
+  value: string;
+  name: string;
+  label: string;
+  onClick: () => void;
+};
+
+type Message = BotMessage | ReplyMessage;
+
+type Props = {
+  messages: Message[];
+};
+
+export const MessageArea: React.VFC<Props> = ({ messages }) => {
+  return (
+    <div>
+      {messages.map(message =>
+        message.messageType === 'bot' ? (
+          <div>
+            <BotMessage text={message.text} />
+          </div>
+        ) : (
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <ReplyMessage
+              type={message.type}
+              value={message.value}
+              name={message.name}
+              label={message.label}
+              onClick={message.onClick}
+            />
+          </div>
+        )
+      )}
+    </div>
+  );
+};
---
+++ src/components/organisms/MessageArea/__stories__/index.stories.tsx
@@ -0,0 +1,41 @@
+import { MessageArea } from '../MessageArea';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'organisms/MessageArea',
+  component: MessageArea,
+};
+
+const Template: ComponentStory<typeof MessageArea> = ({ ...args }) => (
+  <MessageArea {...args} />
+);
+
+const messages = [
+  {
+    messageType: 'bot',
+    text: 'お名前を教えてください。',
+  },
+  {
+    messageType: 'reply',
+    type: 'text',
+    value: '桃子',
+    name: 'name',
+    label: 'お名前',
+  },
+  {
+    messageType: 'bot',
+    text: 'メールアドレスを教えてください。',
+  },
+  {
+    messageType: 'reply',
+    type: 'email',
+    value: 'y@gmail.com',
+    name: 'email',
+    label: 'メールアドレス',
+  },
+];
+
+export const Default = Template.bind({});
+Default.args = {
+  messages,
+};
