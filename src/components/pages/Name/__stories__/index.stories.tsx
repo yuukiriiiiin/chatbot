b68251c9db70451cee0ede415748380fe3a20273
yuki_sakata <sakata_yuuki@waku-2.com>
2021-08-30 16:19:01 +0900

    fix storybook

---
+++ src/components/pages/Name/__stories__/index.stories.tsx
@@ -0,0 +1,34 @@
+import { Name } from '../Name';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'organisms/Name',
+  component: Name,
+};
+
+const Template: ComponentStory<typeof Name> = ({ ...args }) => (
+  <Name {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  form: {
+    name: 'あ',
+    email: '',
+    tel: '',
+    checked: '',
+    message: '',
+  },
+  edited: {
+    name: true,
+    email: false,
+    tel: false,
+    message: false,
+  },
+  validationErrors: {
+    name: { error: false, message: '' },
+    email: { error: false, message: '' },
+    tel: { error: false, message: '' },
+    message: { error: false, message: '' },
+  },
+};
